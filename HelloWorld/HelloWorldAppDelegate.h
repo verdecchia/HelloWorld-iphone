7690f070300f8a4964a27b846bb1d4f342e52584
Marco Verdecchia <marco.verdecchia@gmail.com>
2011-06-10 15:17:19 +0200

    initial project

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by Marco Verdecchia on 6/10/11.
+//  Copyright 2011 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
